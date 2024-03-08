7235944b936a8f6dd8dd212b1363e45f93011065
arevbond <bondarev.n17@yandex.ru>
2024-03-08 10:27:00 +0300

    Refactor cmd, add application

---
+++ cmd/pulse/application/http_server.go
@@ -0,0 +1,35 @@
+package application
+
+import (
+	"github.com/go-chi/chi/v5"
+	"pulse-auth/internal/publicapi"
+)
+
+func (a *App) newHTTPServer(env *env) *HTTPServerWrapper {
+	return NewHTTPServerWrapper(
+		a.Logger,
+		WithAdminServer(a.Config.AdminServer),
+		WithPublicServer(a.Config.PublicServer, a.publicMux(env)),
+	)
+}
+
+func (a *App) publicMux(env *env) *chi.Mux {
+	mux := chi.NewMux()
+
+	handler := publicapi.Handler{
+		Logger:      a.Logger,
+		UserService: env.userService,
+	}
+
+	mux.Post("/login", handler.Login)
+	mux.Post("/user/register", handler.Register)
+	mux.Route("/user", func(r chi.Router) {
+		r.Use(env.authenticationService.AuthenticationInterceptor)
+
+		r.Get("/{userID}", handler.GetUserByID)
+
+		r.Get("/search", handler.SearchUser)
+	})
+
+	return mux
+}
--- migrations/20240306085507_init.sql
+++ migrations/20240306085507_init.sql
@@ -2,14 +2,35 @@
 -- +goose StatementBegin
 SELECT 'up SQL query';
 -- +goose StatementEnd
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS user_table
+(
+    id              TEXT                     NOT NULL,
+    username        TEXT                     NOT NULL,
+    hashed_password TEXT                     NOT NULL,
+    first_name      TEXT,
+    second_name     TEXT,
+    sex             TEXT,
+    birthdate       TIMESTAMP WITH TIME ZONE,
+    biography       TEXT,
+    city            TEXT,
+    created_at      TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_at      TIMESTAMP WITH TIME ZONE,
 
-CREATE TABLE IF NOT EXISTS "users"(
-  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  username VARCHAR(255) NOT NULL UNIQUE,
-  password_hash VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP(0) WITH TIME ZONE
+    CONSTRAINT pk_user_table PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS token_table
+(
+    id         TEXT                     NOT NULL,
+    user_id    TEXT                     NOT NULL,
+    token      TEXT                     NOT NULL,
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    alived_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    CONSTRAINT pk_token_table PRIMARY KEY (id),
+    CONSTRAINT fk_token_table_user_table FOREIGN KEY (user_id) REFERENCES user_table (id)
 );
 -- +goose Down
 -- +goose StatementBegin
