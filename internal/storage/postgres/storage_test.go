94a373a79b84698969b344e311db33d2b2eab916
arevbond <bondarev.n17@yandex.ru>
2024-03-12 10:48:43 +0300

    Add storage token test

--- internal/storage/postgres/storage_test.go
+++ internal/storage/postgres/storage_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"pulse-auth/cmd/pulse/config"
 	"pulse-auth/internal/model"
+	"pulse-auth/internal/token"
 	"pulse-auth/internal/utils"
 	"testing"
 )
@@ -54,7 +55,6 @@ func setup(t *testing.T) {
 
 // TODO: сделать автоматические миграции через goose
 func createTables(t *testing.T) {
-	//fmt.Println(os.Getwd())
 	sqlFile, err := os.Open("../../../migrations/20240306085507_init.sql")
 	if err != nil {
 		t.Fatal("Error opening SQL file:", err)
@@ -81,7 +81,8 @@ func dropTables(t *testing.T) {
 	}
 }
 
-func TestUser(t *testing.T) {
+func TestStorage(t *testing.T) {
+	ctx := context.Background()
 	username := "Nikita"
 	password := "secretPassword"
 	hashedPassword, err := utils.HashPassword(password)
@@ -95,7 +96,7 @@ func TestUser(t *testing.T) {
 		HashedPassword: hashedPassword,
 	}
 
-	userFromDb, err := db.CreateUser(context.Background(), u)
+	userFromDb, err := db.CreateUser(ctx, u)
 	if err != nil {
 		t.Error("can't create user", err)
 	}
@@ -105,16 +106,51 @@ func TestUser(t *testing.T) {
 		HashedPassword: hashedPassword,
 	}
 
-	userFromLogin, err := db.LoginUser(context.Background(), userLogin)
+	userFromLogin, err := db.LoginUser(ctx, userLogin)
 	if err != nil {
 		t.Error("can't user login", err)
 	}
 
-	userById, err := db.GetUserByID(context.Background(), model.UserID(userID))
+	userById, err := db.GetUserByID(ctx, model.UserID(userID))
 	if err != nil {
 		t.Error("can't get uesr by userID", err)
 	}
 
 	assert.Equal(t, userFromDb, userFromLogin)
 	assert.Equal(t, userFromDb, userById)
+
+	// token
+	tokenGenerator := token.NewGenerator(config.ApplicationConfig{
+		SaltValue: "somesalt",
+		App:       "servername",
+	})
+
+	generatedToken, _ := tokenGenerator.GenerateToken(userById)
+
+	tokenID := utils.GenerateUUID()
+	tokenWithMetadata := &model.TokenWithMetadata{
+		TokenID:  tokenID,
+		UserID:   model.UserID(userID),
+		Token:    generatedToken,
+		AlivedAt: tokenGenerator.GetExpirationDate(),
+	}
+
+	createdToken, err := db.CreateToken(ctx, tokenWithMetadata)
+	if err != nil {
+		t.Error("can't create token", err)
+	}
+
+	expectedToken := &model.Token{
+		UserID: model.UserID(userID),
+		Token:  generatedToken,
+	}
+
+	assert.Equal(t, expectedToken, createdToken)
+
+	currentToken, err := db.GetCurrentUserToken(ctx, model.UserID(userID))
+	if err != nil {
+		t.Error("can't get current user token", err)
+	}
+
+	assert.Equal(t, expectedToken, currentToken)
 }
